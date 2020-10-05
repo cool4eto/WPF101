02a69d3744a3ae75fd15baa206e4f2bdd3a73898
Angel Kirov <Angel Kirov@NHRISTOVET>
2020-10-05 17:12:54 +0300

    Value conversion IValueConverter

--- DataBinding/MainWindow.xaml.cs
+++ DataBinding/MainWindow.xaml.cs
@@ -26,27 +26,6 @@ namespace DataBinding
         {
             InitializeComponent();
 
-            users.Add(new User() {Name = "Angel Kirov" });
-            users.Add(new User() { Name = "Mihaela Milcheva" });
-
-            lbUsers.ItemsSource = users;
-        }
-
-        private void btnAddUser_Click(object sender, RoutedEventArgs e)
-        {
-            users.Add(new User { Name = "New user" });
-        }
-
-        private void btnChangeUser_Click(object sender, RoutedEventArgs e)
-        {
-            if (lbUsers.SelectedItem != null)
-                (lbUsers.SelectedItem as User).Name = "Random Name";
-        }
-
-        private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
-        {
-            if (lbUsers.SelectedItem != null)
-                users.Remove(lbUsers.SelectedItem as User);
         }
     }
 }
