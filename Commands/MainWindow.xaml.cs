fe7dd238c1887b81f5a69d58c866cc9d53763625
Angel Kirov <AKirov@nemetschek.bg>
2020-10-06 15:27:33 +0300

    Message Box Dialog

---
+++ Commands/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Commands
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("The new command was invoked");
+        }
+
+
+        private void CutCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            txtEditor.Cut();
+        }
+
+        private void PasteCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            txtEditor.Paste();
+        }
+
+        private void PasteCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = Clipboard.ContainsText();
+        }
+
+        private void CutCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = (txtEditor != null) && (txtEditor.SelectionLength > 0);
+        }
+
+        private void ExitCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void ExitCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ Dialogs/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dialogs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void btnSimpleMessageBox_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello, World!");
+        }
+
+        private void btnMessageBoxWithTitle_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello, World!", "Angel's App");
+        }
+
+        private void btnMessageBoxWithButtons_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello, World!", "Angel's App", MessageBoxButton.OKCancel);
+        }
+
+        private void btnMessageBoxWithResponse_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Say hello to the program!", "Angel's App", MessageBoxButton.YesNoCancel);
+
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    MessageBox.Show("Hi to you too stranger", "Angel's App");
+                    break;
+                case MessageBoxResult.No:
+                    MessageBox.Show("Why are so mean? ", "Angel's App");
+                    break;
+                case MessageBoxResult.Cancel:
+                    MessageBox.Show("As you wish", "Angel's App");
+                    break;
+            }
+        }
+
+        private void btnMessageBoxWithIcon_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello, World!", "Angel's App", MessageBoxButton.OK, MessageBoxImage.Question);
+        }
+
+        private void btnMessageBoxWithDefaultChoice_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello, World!", "Angel's App", MessageBoxButton.YesNoCancel, MessageBoxImage.Question, MessageBoxResult.No);
+        }
+    }
+}
