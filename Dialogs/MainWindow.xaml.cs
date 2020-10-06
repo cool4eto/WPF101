843c7a395009e6bdb896d64daabe88bed6cec716
Angel Kirov <AKirov@nemetschek.bg>
2020-10-06 17:11:14 +0300

    Advanced Dialogs

--- Dialogs/MainWindow.xaml.cs
+++ Dialogs/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -68,6 +70,42 @@ namespace Dialogs
         private void btnMessageBoxWithDefaultChoice_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Hello, World!", "Angel's App", MessageBoxButton.YesNoCancel, MessageBoxImage.Question, MessageBoxResult.No);
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+                txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void btnOpenFiles_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog.Multiselect = true;
+            if(openFileDialog.ShowDialog() ==true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                    txtEditor.AppendText($"{System.IO.Path.GetFullPath(filename)} \n" );
+            }
+        }
+
+        private void btnSaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text file (*.txt)|*.txt|C# file (*.cs)|*.cs";
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, txtEditor.Text);
+        }
+
+        private void btnEnterName_Click(object sender, RoutedEventArgs e)
+        {
+            Input input = new Input("Please enter your name: ", "Angel");
+            if (input.ShowDialog() == true)
+                lblName.Text = input.Answer;
         }
     }
 }
